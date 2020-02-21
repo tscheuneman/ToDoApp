e48019bcbdcd5f8dcf8c86bfe47243f44060775a
Thomas Scheuneman <thomas@tswebvisions.com>
2020-02-20 21:40:55 -0700

    Set new provisioning script

--- Provisioning/scripts/run.sh
+++ Provisioning/scripts/run.sh
@@ -1,9 +1,11 @@
 if [ $1 = "DEV" ]
 then
   echo "This is Dev"
-  cd /var/www
-  mv /usr/local/.dev.env /var/www/.env
-  npx nodemon ./dist/app.js -e ts --exec "npx tsc -p ./tsconfig.json && node /var/www/dist/app.js"
+  cd /var/www/todo
+  npm install
+  mv /usr/local/.dev.env /var/www/todo/.env
+  mv /usr/local/ormconfig.js /var/www/todo/ormconfig.js
+  npx nodemon ./dist/app.js -e ts --exec "npx tsc -p ./tsconfig.json && node /var/www/todo/dist/app.js"
 else
   echo "This is Prod"
 fi
