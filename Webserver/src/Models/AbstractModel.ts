a2641b8853f2d42cc90dfaa39d712ff13292ace3
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-23 17:53:29 -0700

    Add an auto incrementing readable id to abstract model

--- Webserver/src/Models/AbstractModel.ts
+++ Webserver/src/Models/AbstractModel.ts
@@ -1,9 +1,13 @@
-import {PrimaryColumn, BeforeInsert, BaseEntity} from 'typeorm';
+import {PrimaryColumn, BeforeInsert, BaseEntity, Generated, Column} from 'typeorm';
 import {v4 as uuid} from "uuid";
 export abstract class AbstractModel extends BaseEntity{
 
     @PrimaryColumn()
     id: string;
+
+    @Column()
+    @Generated('increment')
+    readable_id: number;
 
     @BeforeInsert()
     private beforeInsert() {
