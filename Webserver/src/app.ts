def55671f0705e2021f2ae2de6514771e7dbe81a
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-23 17:52:30 -0700

    Remove some test logic out of app

--- Webserver/src/app.ts
+++ Webserver/src/app.ts
@@ -1,25 +1,15 @@
 import express from "express";
 import { createConnection } from 'typeorm';
+import * as ormConfig from '../ormconfig';
 import "reflect-metadata";
-import {UserService} from "./Services/UserService";
 
-const User = new UserService();
 
 const APIRoutes = require('./Api/routes');
 require('dotenv').config();
 
 const app = express();
 
-createConnection().then(async (connection) => {
-
-  const userInsert = User.insert({
-    username: 'tjscheun',
-    email: 'thomas@tswebvisions.com',
-    password: 'sdfsdfsddsf'
-  });
-
-  console.log(userInsert);
-
+createConnection(ormConfig).then(async (connection) => {
   app.get('/', (req: any, res) => {
       res.send('Base Route');
   });
