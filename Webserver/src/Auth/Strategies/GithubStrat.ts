6deb75c6eb0b9b7cd0f47f05aa8a70bbf7eb29f6
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-25 15:43:17 -0700

    Fix callback on github strat

--- Webserver/src/Auth/Strategies/GithubStrat.ts
+++ Webserver/src/Auth/Strategies/GithubStrat.ts
@@ -1,13 +1,25 @@
 import GitHubStrategy from 'passport-github';
+import UserService from '../../Services/UserService';
 
 export default function GithubStrat() {
-    return new GitHubStrategy.Strategy('github', {
-        clientId: process.env.GoogleID,
-        clientSecret: process.env.GoogleSecret,
-        callbackURL: 'http://127.0.0.1/login/github/callback'
+    return new GitHubStrategy.Strategy({
+        clientID: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+        callbackURL: 'http://127.0.0.1:5000/login/github/callback'
     }, (token, refreshToken, profile, done) => {
-        process.nextTick(() => {
-            //DO Stuff
+        process.nextTick(async () => {
+            try {
+                const User = new UserService;
+                const UserObj = {
+                    thirdParty: true,
+                    uniqueId: profile.id,
+                    username: profile.username,
+                }
+                const ReturnUser = await User.findOrCreate(UserObj);
+                return done(null, ReturnUser);
+            } catch(err) {
+                return done(err, false);
+            }
         });
     });
 }
