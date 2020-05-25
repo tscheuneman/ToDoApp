ef8e80c22ab63f97c747e309c96bd4a0b9622e94
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-25 15:43:52 -0700

    Update AuthRoutes.ts

--- Webserver/src/Auth/Routes/AuthRoutes.ts
+++ Webserver/src/Auth/Routes/AuthRoutes.ts
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router.get('/github', passport.authenticate('github'));
 router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/error' }),
-  function(req, res){
-    res.send('Logged In github.');
+  (req, res) => {
+    console.log('wut?');
+    res.redirect('/');
 });
 
 
