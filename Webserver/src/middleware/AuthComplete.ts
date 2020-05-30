5a701bc8d5b8784a0be363a0498e23eb575d024a
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-30 15:58:45 -0700

    Create middleware to verify and get token values

--- Webserver/src/middleware/AuthComplete.ts
+++ Webserver/src/middleware/AuthComplete.ts
@@ -3,9 +3,18 @@ import jwt from 'jsonwebtoken';
 export default function IsAuth(req, res, next) {
     if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
         const jwToken = req.headers.authorization.split(' ')[1];
-        const token = jwt.verify(jwToken, process.env.JWT_SECRET);
-        req.user = token;
-        next();
+        jwt.verify(jwToken, process.env.JWT_SECRET, (err, token) => {
+            if(err) {
+                res.send({
+                    success: false,
+                    error: err
+                });
+            } else {
+                req.user = token;
+                next();
+            }
+        });
+
     } else {
         res.send({success: false});
     }
