bf59df297491a472159065f8264dd725ec69e279
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-29 23:21:57 -0700

    Category types

--- Webserver/src/@types/Category.d.ts
+++ Webserver/src/@types/Category.d.ts
@@ -3,3 +3,15 @@ interface CategoryInsertDO {
     slug: string,
     user: string
 }
+
+interface CategoryEdit {
+    id: string,
+    name: string,
+    slug: string,
+    user: string
+}
+
+interface CategoryDelete {
+    id: string,
+    user: string
+}
