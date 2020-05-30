eb6e9ddb68a3d70ff2a77c93563c71171f5cbc83
Thomas Scheuneman <thomas@tswebvisions.com>
2020-05-29 23:21:34 -0700

    Change auth middleware

--- Webserver/src/middleware/AuthComplete.ts
+++ Webserver/src/middleware/AuthComplete.ts
@@ -1,7 +1,12 @@
+import jwt from 'jsonwebtoken';
+
 export default function IsAuth(req, res, next) {
-    if(req.user !== undefined) {
+    if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        const jwToken = req.headers.authorization.split(' ')[1];
+        const token = jwt.verify(jwToken, process.env.JWT_SECRET);
+        req.user = token;
         next();
     } else {
-        res.redirect('/login');
+        res.send({success: false});
     }
 }
